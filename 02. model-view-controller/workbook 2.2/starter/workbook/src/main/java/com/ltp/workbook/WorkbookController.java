cc2f5594dfaf677377e20965270fbfe8233d8414
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-12-05 16:37:32 -0600

    workbook 2.2 solved

--- 02. model-view-controller/workbook 2.2/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
+++ 02. model-view-controller/workbook 2.2/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
@@ -1,13 +1,19 @@
 package com.ltp.workbook;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WorkbookController {
-    
+
     @GetMapping("/")
-    public String getShows() {
+    public String getShows(Model model) {
+        model.addAttribute("first", new Show("Breaking Bad", "Ozymandias", 10.0));
+        model.addAttribute("second", new Show("Attack on Titan", "Hero", 9.9));
+        model.addAttribute("third", new Show("Attack on Titan", "Perfect Game", 9.9));
+        model.addAttribute("fourth", new Show("Star Wars: The Clone Wars", "Victory and Death", 9.9));
+        model.addAttribute("fifth", new Show("Mr. Robot", "407 Proxy Authentication Required", 9.9));
         return "shows";
     }
 
