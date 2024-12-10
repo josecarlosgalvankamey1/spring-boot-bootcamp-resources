2597b3ffb06516c3c60acf829bb6a52bfe755888
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-12-10 12:34:43 -0600

    Thymeleaft loops

--- 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,5 +1,8 @@
 package com.ltp.gradesubmission;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GradeController {
 
+    List<Grade> studentGrades = Arrays.asList(
+            (new Grade("Harry", "Potions", "C-")),
+            (new Grade("Hermione", "Arithmancy", "A+")),
+            (new Grade("Neville", "Charms", "A-")));
+
     @GetMapping("/grades")
     public String getGrades(Model model) {
-        Grade grade = new Grade("Harry", "Potions", "C-");
-        model.addAttribute("grade", grade);
+        model.addAttribute("grades", studentGrades);
         return "grades";
     }
 }
