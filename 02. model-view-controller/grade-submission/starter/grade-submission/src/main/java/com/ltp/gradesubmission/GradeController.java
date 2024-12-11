aa9feeed260cf24f621b6086b49e9b84a0aab29f
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-12-11 12:44:20 -0600

    form submission

--- 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -20,4 +20,10 @@ public class GradeController {
         model.addAttribute("grades", studentGrades);
         return "grades";
     }
+
+    @GetMapping("/")
+    public String gradeForm(Model model) {
+        model.addAttribute("grade", new Grade());
+        return "form";
+    }
 }
