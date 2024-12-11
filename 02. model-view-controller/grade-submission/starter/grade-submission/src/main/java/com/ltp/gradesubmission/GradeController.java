10acabf4a144e284d959e4fd706e2a8058395b4d
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-12-11 16:34:57 -0600

    Form submission

--- 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GradeController {
@@ -20,14 +21,31 @@ public class GradeController {
     }
 
     @GetMapping("/")
-    public String gradeForm(Model model) {
-        model.addAttribute("grade", new Grade());
+    public String gradeForm(Model model, @RequestParam(required = false) String name) {
+        Grade grade;
+        int index = getGradeIndex(name);
+        grade = index == -1 ? new Grade() : studentGrades.get(index);
+        model.addAttribute("grade", grade);
         return "form";
     }
 
     @PostMapping("/handleSubmit")
     public String submitForm(Grade grade) {
-        studentGrades.add(grade);
+        int index = getGradeIndex(grade.getName());
+        if (index == -1) {
+            studentGrades.add(grade);
+        } else {
+            studentGrades.set(index, grade);
+        }
         return "redirect:/grades";
+    }
+
+    public int getGradeIndex(String name) {
+        for (int i = 0; i < studentGrades.size(); i++) {
+            Grade currentGrade = studentGrades.get(i);
+            if (currentGrade.getName().equals(name))
+                return i;
+        }
+        return -1;
     }
 }
