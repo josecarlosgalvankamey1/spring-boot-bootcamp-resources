e66ac2e6237ce1a7d66bc62771544158c1e23b6a
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-12-13 15:59:37 -0600

    workbook 3.1 started

---
+++ 03. field-validation/workbook 3.1/starter/workbook/src/main/java/com/example/workbook/WorkbookController.java
@@ -0,0 +1,13 @@
+package com.example.workbook;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WorkbookController {
+
+    @GetMapping("/")
+    public String getForm() {
+        return "sign-up";
+    }
+}
