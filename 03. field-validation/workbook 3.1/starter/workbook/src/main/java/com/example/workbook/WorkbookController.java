005fb0222d33682fafff82366cb79d126d85c60e
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-12-18 13:14:55 -0600

    workbook 3.1 - task 10 solved

--- 03. field-validation/workbook 3.1/starter/workbook/src/main/java/com/example/workbook/WorkbookController.java
+++ 03. field-validation/workbook 3.1/starter/workbook/src/main/java/com/example/workbook/WorkbookController.java
@@ -2,6 +2,7 @@ package com.example.workbook;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WorkbookController {
@@ -14,5 +15,11 @@ public class WorkbookController {
     @GetMapping("/result")
     public String getResult() {
         return "result";
+    }
+
+    @PostMapping("/submitItem")
+    public String handleSubmit(String userName) {
+
+        return "redirect:result";
     }
 }
