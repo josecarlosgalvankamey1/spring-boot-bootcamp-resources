ea7354c60182084a9092f764bb017d3d7bc6ebdc
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-12-18 13:08:52 -0600

    workbook 3.1 - task 7 solved

---
+++ 03. field-validation/workbook 3.1/starter/workbook/src/main/java/com/example/workbook/User.java
@@ -0,0 +1,64 @@
+package com.example.workbook;
+
+import java.sql.Date;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String email;
+    private Date dateOfBirth;
+
+    public User() {
+
+    }
+
+    public User(String firstName, String lastName, String userName, String email, Date dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+}
