27a65607bb7f4e5fc28da56b94af47fa8a5f4020
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-12-21 11:20:30 -0600

    three layer - repository and controller lesson

---
+++ 04. three-layer-codebase/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/repository/GradeRepository.java
@@ -0,0 +1,26 @@
+package com.ltp.gradesubmission.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ltp.gradesubmission.Grade;
+
+public class GradeRepository {
+    private List<Grade> studentGrades = new ArrayList<>();
+
+    public Grade getGrade(int id) {
+        return studentGrades.get(id);
+    }
+
+    public void addGrade(Grade grade) {
+        studentGrades.add(grade);
+    }
+
+    public void updateGrade(int index, Grade grade) {
+        studentGrades.set(index, grade);
+    }
+
+    public List<Grade> getGrades() {
+        return studentGrades;
+    }
+}
