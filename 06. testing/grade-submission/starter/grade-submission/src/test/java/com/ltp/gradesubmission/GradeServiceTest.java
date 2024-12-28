fd811a01ca48d921eb93b119b4e6b9679a526ead
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-12-27 19:45:51 -0600

    Testing - get grades method test

--- 06. testing/grade-submission/starter/grade-submission/src/test/java/com/ltp/gradesubmission/GradeServiceTest.java
+++ 06. testing/grade-submission/starter/grade-submission/src/test/java/com/ltp/gradesubmission/GradeServiceTest.java
@@ -1,10 +1,18 @@
 package com.ltp.gradesubmission;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import com.ltp.gradesubmission.pojo.Grade;
 import com.ltp.gradesubmission.repository.GradeRepository;
 import com.ltp.gradesubmission.service.GradeService;
 
@@ -16,4 +24,16 @@ public class GradeServiceTest {
 
     @InjectMocks
     private GradeService gradeService;
+
+    @Test
+    public void getGradesFromRepoTest() {
+        when(gradeRepository.getGrades()).thenReturn(Arrays.asList(
+                new Grade("Harry", "Potions", "C+"),
+                new Grade("Hermione", "Arithmancy", "A+")));
+
+        List<Grade> result = gradeService.getGrades();
+
+        assertEquals("Harry", result.get(0).getName());
+        assertEquals("Arithmancy", result.get(1).getSubject());
+    }
 }
