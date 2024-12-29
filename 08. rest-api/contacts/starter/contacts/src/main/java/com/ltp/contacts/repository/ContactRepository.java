6bc5043e39621aab43cd17b52e5f207591a37d03
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2024-12-29 16:25:34 -0600

    API REST - GET

--- 08. rest-api/contacts/starter/contacts/src/main/java/com/ltp/contacts/repository/ContactRepository.java
+++ 08. rest-api/contacts/starter/contacts/src/main/java/com/ltp/contacts/repository/ContactRepository.java
@@ -1,6 +1,6 @@
 package com.ltp.contacts.repository;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
@@ -9,8 +9,11 @@ import com.ltp.contacts.pojo.Contact;
 
 @Repository
 public class ContactRepository {
-    
-    private List<Contact> contacts = new ArrayList<>();
+
+    private List<Contact> contacts = Arrays.asList(
+            new Contact("123", "Jon Snow", "0001112222"),
+            new Contact("456", "Tyrion", "8887773344"),
+            new Contact("789", "The Hound", "5558883344"));
 
     public List<Contact> getContacts() {
         return contacts;
@@ -24,10 +27,10 @@ public class ContactRepository {
         contacts.add(contact);
     }
 
-    public void updateContact(int index, Contact contact) { 
-        contacts.set(index, contact); 
+    public void updateContact(int index, Contact contact) {
+        contacts.set(index, contact);
     }
-    
+
     public void deleteContact(int index) {
         contacts.remove(index);
     }
--- 08. rest-api/contacts/starter/contacts/src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
+++ 08. rest-api/contacts/starter/contacts/src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.stream.IntStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.repository.ContactRepository;
 
 @Service
@@ -12,15 +13,18 @@ public class ContactServiceImpl implements ContactService {
 
     @Autowired
     private ContactRepository contactRepository;
-    
 
-
+    @Override
+    public Contact geContactById(String id) {
+        int index = findIndexById(id);
+        return contactRepository.getContact(index);
+    }
 
     private int findIndexById(String id) {
         return IntStream.range(0, contactRepository.getContacts().size())
-            .filter(index -> contactRepository.getContacts().get(index).getId().equals(id))
-            .findFirst()
-            .orElseThrow();
+                .filter(index -> contactRepository.getContacts().get(index).getId().equals(id))
+                .findFirst()
+                .orElseThrow();
     }
 
 }
