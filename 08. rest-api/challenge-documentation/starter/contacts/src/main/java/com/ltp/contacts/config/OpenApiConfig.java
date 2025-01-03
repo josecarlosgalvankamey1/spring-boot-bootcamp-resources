bd773295c622d97c7c666cacd3c001a9df35df8b
josecarlosgalvankamey1 <josecarlosgalvankamey1@gmail.com>
2025-01-03 10:06:47 -0600

    API REST - Documentation

--- 08. rest-api/challenge-documentation/starter/contacts/src/main/java/com/ltp/contacts/config/OpenApiConfig.java
+++ 08. rest-api/challenge-documentation/starter/contacts/src/main/java/com/ltp/contacts/config/OpenApiConfig.java
@@ -4,11 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
 public class OpenApiConfig {
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI();
+        return new OpenAPI().info(
+                new Info()
+                        .title("Contact API")
+                        .version("1.0")
+                        .description("An API that can manage contacts"));
     }
 }
